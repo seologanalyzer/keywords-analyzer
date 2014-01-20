74aa5f836445871bdba20293cd879204f7795604
Pierre Grimaud <grimaud.pierre@gmail.com>
2014-01-20 11:08:07 +0100

    [Core] Bootstrap for crons

---
+++ lib/cron/adwords.php
@@ -0,0 +1,24 @@
+<?php
+
+$out = array();
+$x = exec('ps -ef | grep -v grep | grep ' . $_SERVER['PHP_SELF'] . ' | awk \'{print $2}\' ', $out);
+if (sizeof($out) > 2):
+  echo 'Already launched';
+  exit;
+endif;
+
+require_once dirname(__FILE__) . '/bootstrap.php';
+
+Connexion::getInstance()->query("SELECT keyword, id_keyword
+																FROM keyword
+																WHERE search = 0 ");
+
+$keywords = Connexion::getInstance()->fetchAll();
+
+show($keywords);
+
+foreach ($keywords as $keyword):
+
+  new Adwords($keyword);
+
+endforeach;
--- lib/cron/bootstrap.php
+++ lib/cron/bootstrap.php
@@ -1,3 +1,22 @@
 <?php
 
-//todo for adwords search
+set_time_limit(0);
+ini_set('memory_limit', -1);
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+header('Content-Type: text/html; charset=utf-8');
+
+define('ROOT_PATH', dirname(__FILE__).'/../../');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_BASE', '');
+define('PHP_ENV', php_sapi_name());
+//Running cron by CLI ? Server IP
+define('DB_HOST','localhost');
+
+require_once ROOT_PATH.'lib/functions.php';
+spl_autoload_register('defaultAutoload');
+
+//Adwords API
+require_once ROOT_PATH.'lib/adwords/init.php';
