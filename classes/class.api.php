2517a5e047ca014c0723a7f0e54fb15c3a764afc
Pierre Grimaud <grimaud.pierre@gmail.com>
2014-01-21 20:52:21 +0100

    [API] get/set for positions

--- classes/class.api.php
+++ classes/class.api.php
@@ -42,11 +42,35 @@ class Api {
     echo json_encode($keyword);
     exit;
   }
-  
+
+  public function setposition() {
+    if (isset($this->post['id_keyword']) && isset($this->post['values'])):
+      $positions = unserialize(urldecode(base64_decode($this->post['values'])));
+      foreach ($positions as $k => $position):
+        $xpl = explode('&amp;', $position['url']);
+        Connexion::getInstance()->query("SELECT id_url FROM url WHERE url = '" . $xpl[0] . "' ");
+        $id_url = Connexion::getInstance()->result();
+        if (!$id_url):
+          Connexion::getInstance()->query("INSERT INTO url (url) VALUES ('" . $xpl[0] . "') ");
+          Connexion::getInstance()->query("SELECT id_url FROM url WHERE url = '" . $xpl[0] . "' ");
+          $id_url = Connexion::getInstance()->result();
+        endif;
+        Connexion::getInstance()->query("INSERT INTO position (id_url, position, date, id_keyword) VALUES ('" . $id_url . "', '" . ($k + 1) . "', '" . date('Y-m-d') . "', '" . $this->post['id_keyword'] . "') ");
+      endforeach;
+    endif;
+  }
+
   public function getposition() {
-    //
-    $date = new DateTime();
-    echo json_encode($keyword);
+    Connexion::getInstance()->query("SELECT id_keyword, keyword FROM keyword WHERE id_keyword NOT IN (SELECT id_keyword FROM position) AND id_user = '" . $this->user['id'] . "' LIMIT 0,1 ");
+    $keyword = Connexion::getInstance()->fetch();
+    if (isset($keyword['id_keyword'])):
+      $keyword['url'] = $this->user['url'];
+      $keyword['gg'] = $this->user['gg'];
+      $keyword['delay'] = $this->user['delay'];
+      echo json_encode($keyword);
+    else:
+
+    endif;
     exit;
   }
 
--- lib/launcher/update.php
+++ lib/launcher/update.php
@@ -76,7 +76,70 @@ if (isset($keyword->keyword)) {
 $position = getposition($token);
 
 if (isset($position->keyword)) {
-  
+  while ((isset($position->keyword))):
+    $positions = array();
+
+    for ($start = 0; $start < 10; $start++):
+
+      echo @date('Y-m-d H:i:s') . ' - Position : ' . $position->keyword . ' (Page ' . ($start + 1) . ')' . "\n";
+
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      curl_setopt($ch, CURLOPT_URL, 'https://www.google.' . $position->gg . '/search?hl=fr&gl=fr&q=' . urlencode($position->keyword) . '&ie=utf-8&oe=utf-8&client=firefox-a&gws_rd=cr&ei=KJrGUvrsNeL30gXn6IHQDg&pws=0&start=' . ($start * 10 ));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+      curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; fr-FR; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 GTB5');
+      $response = curl_exec($ch);
+      curl_close($ch);
+
+      $page = utf8_decode($response);
+      $page = str_replace(array('<em>', '</em>', '<b>', '</b>'), '', $page);
+
+      preg_match_all('@<h3\s*class=[^<>]*>\s*<a[^<>]*href="([^<>]*)"[^<>]*>(.*)</a>\s*</h3>@siU', $page, $results);
+
+      $titles = $results[2];
+      $urls = $results[1];
+      //clean images
+      foreach ($urls as $k => $url):
+        if (strpos($url, '/url?q') !== false):
+          $positions[] = array('url' => str_replace('/url?q=', '', $url), 'title' => $titles[$k]);
+        endif;
+      endforeach;
+
+      for ($i = rand($position->delay, ($position->delay + 3)); $i > 0; $i--):
+        echo '.';
+        sleep(1);
+      endfor;
+
+    endfor;
+
+    //call post to the api
+    //curl to webservice
+    $ch = curl_init();
+    $fields = array(
+        'token' => $token,
+        'id_keyword' => urlencode($position->id_keyword),
+        'values' => base64_encode(urlencode(serialize($positions)))
+    );
+    $fields_string = '';
+    foreach ($fields as $key => $value):
+      $fields_string .= $key . '=' . $value . '&';
+    endforeach;
+
+    rtrim($fields_string, '&');
+
+    curl_setopt($ch, CURLOPT_URL, 'http://keywords.dulol.fr/api/setposition');
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+
+    $result = curl_exec($ch);
+
+    curl_close($ch);
+
+    $position = getposition($token);
+
+  endwhile;
 }
 
 //if no queue
