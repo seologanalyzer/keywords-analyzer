beb9d3740d15564d69c9ede679dc2ac40de12876
Pierre Grimaud <grimaud.pierre@gmail.com>
2014-01-23 20:08:47 +0100

    [Core] Add 404 page

---
+++ views/404.php
@@ -0,0 +1,12 @@
+<?php add('header'); ?>
+<!-- Content -->
+<div id="contentwrapper">
+  <div class="main_content">
+    <div class="row-fluid" style="margin-top:10px;" id="parameters">
+      <div class="span12 alert alert-danger" style="margin:0px;">
+        ERREUR 404
+      </div>
+    </div>
+  </div>
+</div>
+<?php add('footer');?>
